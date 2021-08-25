31dd2c3fec70cea09c975e5d9565fecfe2fbe15c
Sheralram <ramkrishna.sheral@gmail.com>
2021-08-25 19:39:11 +0530

    Added: Max method to take more than 3 parameters

--- src/com/Bridgelabz/max/FindMax.java
+++ src/com/Bridgelabz/max/FindMax.java
@@ -1,29 +1,32 @@
 package com.Bridgelabz.max;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class FindMax <T extends Comparable<T>> {
-    T input1, input2, input3;
-        public FindMax(T input1, T input2, T input3) {
-            this.input1 = input1;
-            this.input2 = input2;
-            this.input3 = input3;
+
+    ArrayList<T> list = new ArrayList<T>();
+    //contructor accept multiple arguments
+    FindMax(T... inputs){
+        for (T value : inputs){
+            this.list.add(value);
         }
+    }
 
         public T testMaximum() {
-            return FindMax.getMax(input1,input2,input3);
+            return FindMax.testMaximum(list);
         }
 
-
-
-
-    public static <T extends Comparable<T>> T getMax(T num1,T num2,T num3){         //Refactor all 3 in one Generic Method
-        T max=num1;
-        if(num2.compareTo(max)>0)
-            max=num2;
-        if(num3.compareTo(max)>0)
-            max=num3;
-        showMaxValue(num1, num2, num3, max);
-        return max;
+    public static <T extends Comparable<T>> T testMaximum(ArrayList<T> list) {
+        // Sort the list in ascending order
+        Collections.sort(list);
+        // last position holds the max value
+        T maxValue = list.get(list.size() - 1);
+        System.out.println("Maximum : " + maxValue);
+        return maxValue;
     }
+
+
 
 
     private static <T extends Comparable<T>> void showMaxValue(T num1, T num2, T num3, T max) {
@@ -31,14 +34,14 @@ public class FindMax <T extends Comparable<T>> {
         System.out.println("Maximum : " + max+"\n");
     }
     public static void main(String[] args) {
-        Integer intValue1 = 300 , intValue2 = 900 , intValue3 = 60;
-        Float floatValue1 = 9.3f , floatValue2 = 3.5f , floatValue3 = 8.7f;
-        String stringValue1 = "Mango" , stringValue2 = "Orange" , stringValue3 = "PineApple";
+        Integer intValue1 = 300 , intValue2 = 900 , intValue3 = 60, intValue=270;
+        Float floatValue1 = 9.3f , floatValue2 = 3.5f , floatValue3 = 8.7f, floatValue = 9.6f;
+        String stringValue1 = "Mango" , stringValue2 = "Orange" , stringValue3 = "PineApple" , stringValue4 = "apple";
 
 
-        new FindMax<>(intValue1, intValue2, intValue3).testMaximum();
-        new FindMax<>(floatValue1, floatValue2, floatValue3).testMaximum();
-        new FindMax<>(stringValue1, stringValue2, stringValue3).testMaximum();
+        new FindMax<>(intValue1, intValue2, intValue3,intValue).testMaximum();
+        new FindMax<>(floatValue1, floatValue2, floatValue3,floatValue).testMaximum();
+        new FindMax<>(stringValue1, stringValue2, stringValue3,stringValue4).testMaximum();
     }
 
 }

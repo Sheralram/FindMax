6c711fd655113c1f7004a639e9284db0743d3d39
Sheralram <ramkrishna.sheral@gmail.com>
2021-08-25 18:48:07 +0530

    Check Maximum for 3 Integers

--- src/com/Bridgelabz/max/FindMax.java
+++ src/com/Bridgelabz/max/FindMax.java
@@ -1,7 +1,22 @@
 package com.Bridgelabz.max;
 
 public class FindMax {
+
+    public Integer maxNum(Integer input1, Integer input2, Integer input3){
+        Integer max = input1;
+        if(input2.compareTo(max) > 0)
+            max = input2;
+        if(input3.compareTo(max) > 0)
+            max = input3;
+        System.out.println("maximum of three integer is "+max);
+        return max;
+    }
+
+
     public static void main(String[] args) {
         System.out.println("Welcome to Find Maximum using Generics");
+      FindMax maximum=new FindMax();
+        maximum.maxNum(2,5,8);
+
     }
 }
